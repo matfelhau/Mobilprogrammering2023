2bbec773d35795492a7d7355edce686bbdcfcac1
Fredrik <fredrk@hiof.no>
2023-10-13 21:15:28 +0200

    Added spinner options for spice level and recipe difficulty in CreateRecipeActivity

--- app/src/main/java/com/example/matapp/CreateRecipeActivity.kt
+++ app/src/main/java/com/example/matapp/CreateRecipeActivity.kt
@@ -2,6 +2,8 @@ package com.example.matapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
 import com.example.matapp.databinding.ActivityCreaterecipeBinding
 
@@ -35,5 +37,17 @@ class CreateRecipeActivity : ComponentActivity() {
             startActivity(intent)
             finish()
         }
+
+        val options = arrayOf( 1, 2, 3)
+
+        val spinnerDifficulty: Spinner = findViewById(R.id.spinnerRecipeDifficulty)
+        val spinnerSpice: Spinner = findViewById(R.id.spinnerSpiceLevel)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerSpice.adapter = adapter
+        spinnerDifficulty.adapter = adapter
+
     }
 }
