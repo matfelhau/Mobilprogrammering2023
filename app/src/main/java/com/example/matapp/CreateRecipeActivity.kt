2ddf2aa25a427f9b02f6fb0bd9cb296ae2b93b4f
Fredrik <fredrk@hiof.no>
2023-10-13 17:41:00 +0200

    Made Create Recipe UI

--- app/src/main/java/com/example/matapp/CreateRecipeActivity.kt
+++ app/src/main/java/com/example/matapp/CreateRecipeActivity.kt
@@ -1,12 +1,46 @@
 package com.example.matapp
 
+import android.R
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.activity.ComponentActivity
-import com.example.matapp.ui.theme.MatappTheme
+import com.example.matapp.databinding.ActivitySearchBinding
+
 
 class CreateRecipeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imageButtonSearch.setOnClickListener {
+            val intent = Intent(this, SearchActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.imageButtonSaved.setOnClickListener {
+            val intent = Intent(this, SavedActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.imageButtonForYou.setOnClickListener {
+            val intent = Intent(this, ForYouActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.imageButtonSettings.setOnClickListener {
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
+
+
 }
