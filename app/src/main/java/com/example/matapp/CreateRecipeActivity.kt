5af95e7a438d2c0dc0a460136f7082f2a5fc6495
matfelhau <mathiashaugen02@gmail.com>
2023-10-14 03:38:41 +0200

    Matapp, omstrukturerte koden for å gjøre den mer modulær og enklere å lese, skal gjøre dette med alle.

--- app/src/main/java/com/example/matapp/CreateRecipeActivity.kt
+++ app/src/main/java/com/example/matapp/CreateRecipeActivity.kt
@@ -2,20 +2,46 @@ package com.example.matapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.Spinner
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import com.example.matapp.databinding.ActivityCreaterecipeBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 
 class CreateRecipeActivity : ComponentActivity() {
+
+    private var dataAdded = false
+    private lateinit var recipeId: String
+    private val database by lazy { FirebaseDatabase.getInstance().getReference("recipes") }
+    private val userId by lazy { FirebaseAuth.getInstance().currentUser?.uid }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        recipeId = database.push().key ?: return showError("Error initializing recipe.")
+        initializeRecipe()
 
         val binding = ActivityCreaterecipeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setupUI(binding)
+    }
+
+    private fun initializeRecipe() {
+        val initialRecipeData = mapOf(
+            "userId" to userId,
+            "title" to "",
+            "cookTime" to "",
+            "difficulty" to "",
+            "spiceLevel" to ""
+        )
+        database.child(recipeId).setValue(initialRecipeData)
+    }
+
+    private fun setupUI(binding: ActivityCreaterecipeBinding) {
         binding.imageButtonSearch.setOnClickListener {
             val intent = Intent(this, SearchActivity::class.java)
             startActivity(intent)
@@ -41,48 +67,120 @@ class CreateRecipeActivity : ComponentActivity() {
         }
 
         val difficultyOptions = arrayOf("Easy", "Medium", "Hard")
-        val spiceOptions = arrayOf("Mild" , "Medium", "Strong")
-        val quantity = arrayOf("grams", "dl", "tsp", "tbsp", "slices", "pcs")
+        val spiceOptions = arrayOf("Mild", "Medium", "Strong")
+        val measuringUnitOptions = arrayOf("grams", "dl", "tsp", "tbsp", "slices", "pcs")
 
-        val spinnerDifficulty: Spinner = findViewById(R.id.spinnerRecipeDifficulty)
-        val spinnerSpice: Spinner = findViewById(R.id.spinnerSpiceLevel)
-        val spinnerQuantity: Spinner = findViewById(R.id.spinnerIngredientQuantity)
+        val spinnerDifficulty: Spinner = binding.spinnerRecipeDifficulty
+        val spinnerSpice: Spinner = binding.spinnerSpiceLevel
+        val spinnerQuantity: Spinner = binding.spinnerIngredientQuantity
 
-        val difficultyAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, difficultyOptions)
+        val difficultyAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, difficultyOptions)
         difficultyAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         val spiceAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, spiceOptions)
         spiceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        val adapterQuantity = ArrayAdapter(this, android.R.layout.simple_spinner_item, quantity)
+        val adapterQuantity =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, measuringUnitOptions)
         adapterQuantity.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spinnerSpice.adapter = spiceAdapter
         spinnerDifficulty.adapter = difficultyAdapter
         spinnerQuantity.adapter = adapterQuantity
 
+
+        binding.addIngredientButton.setOnClickListener {
+            addIngredient(binding)
+        }
+
         binding.createRecipeButton.setOnClickListener {
-            val recipeTitle = binding.editTextRecipeTitle.text.toString()
-            if (recipeTitle.isEmpty()) {
-                Toast.makeText(this, "Please enter a title for your recipe.", Toast.LENGTH_SHORT).show()
-            }
-            val cookTime = binding.editTextRecipeCookTime.text.toString()
-            if (cookTime.isEmpty()) {
-                Toast.makeText(this, "Please enter a cooktime for your recipe.", Toast.LENGTH_SHORT).show()
-            }
-            val ingredient = binding.editTextRecipeIngredients.text.toString()
-            if (ingredient.isEmpty()) {
-                Toast.makeText(this, "Please enter a ingredient for you recipe.", Toast.LENGTH_SHORT).show()
-            }
-            //add quantity input to specify to the database
-            val difficulty = binding.spinnerRecipeDifficulty.selectedItem.toString()
-            if (difficulty.isEmpty()) {
-                Toast.makeText(this, "Please enter a difficulty for your recipe.", Toast.LENGTH_SHORT).show()
-            }
-            val spiceLevel = binding.spinnerSpiceLevel.selectedItem.toString()
-            if (spiceLevel.isEmpty()) {
-                Toast.makeText(this, "Please enter a spice level for your recipe.", Toast.LENGTH_SHORT).show()
-            }
+            createRecipe(binding)
         }
     }
+
+    private fun addIngredient(binding: ActivityCreaterecipeBinding) {
+        val ingredient = binding.editTextRecipeIngredients.text.toString()
+        val quantity = binding.editTextIngredientQuantity.text.toString()
+        val measuringUnit = binding.spinnerIngredientQuantity.selectedItem.toString()
+
+        if (ingredient.isEmpty() || quantity.isEmpty()) {
+            showError("Please fill out all ingredient fields.")
+            return
+        }
+
+        val ingredientData = mapOf(
+            "name" to ingredient,
+            "quantity" to quantity,
+            "unit" to measuringUnit,
+        )
+
+        recipeId?.let {
+            database.child(it).child("ingredients").push().setValue(ingredientData)
+                .addOnSuccessListener {
+                    Log.d("FirebaseDebug", "Ingredient added successfully!")
+                    binding.editTextRecipeIngredients.text.clear()
+                    binding.editTextIngredientQuantity.text.clear()
+                    dataAdded = true
+                }.addOnFailureListener { exception ->
+                    Log.e("FirebaseDebug", "Error adding ingredient: ", exception)
+                }
+        } ?: showError("Error adding ingredient.")
+    }
+
+    private fun createRecipe(binding: ActivityCreaterecipeBinding) {
+        val recipeTitle = binding.editTextRecipeTitle.text.toString()
+        if (recipeTitle.isEmpty()) {
+            showError("Please enter a title for your recipe.")
+            return
+        }
+        val cookTime = binding.editTextRecipeCookTime.text.toString()
+        if (cookTime.isEmpty()) {
+            showError("Please enter a cook time for your recipe.")
+        }
+
+        val difficulty = binding.spinnerRecipeDifficulty.selectedItem.toString()
+        if (difficulty.isEmpty()) {
+            showError("Please enter a difficulty for your recipe.")
+        }
+        val spiceLevel = binding.spinnerSpiceLevel.selectedItem.toString()
+        if (spiceLevel.isEmpty()) {
+            showError("Please enter a spice level for your recipe.")
+        }
+
+        val isVegan = binding.veganCheckbox.isChecked
+        val allergens = mapOf(
+            "gluten" to binding.glutenCheckbox.isChecked,
+            "nuts" to binding.nutsCheckbox.isChecked,
+            "soy" to binding.soyCheckbox.isChecked
+        )
+        val recipeData = mapOf(
+            "userId" to userId,
+            "title" to recipeTitle,
+            "cookTime" to cookTime,
+            "difficulty" to difficulty,
+            "spiceLevel" to spiceLevel,
+            "isVegan" to isVegan,
+            "allergens" to allergens
+        )
+
+        database.child(recipeId!!).updateChildren(recipeData).addOnSuccessListener {
+            Log.d("FirebaseDebug", "Data written successfully!")
+            dataAdded = true
+        }.addOnFailureListener { exception ->
+            Log.e("FirebaseDebug", "Error writing data: ", exception)
+        }
+    }
+
+    private fun showError(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onBackPressed() {
+        if (!dataAdded) {
+            database.child(recipeId).removeValue()
+        }
+        super.onBackPressed()
+    }
 }
+
